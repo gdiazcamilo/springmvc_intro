c5e284a2f2be986a3887f62a32ff2ad8f41a3c20
gdiazcamilo <gdiazcamilo@gmail.com>
2015-08-18 00:48:20 -0400

    using model, serving static resources

---
+++ FitnessTracker/src/main/java/local/gdiaz/fitnesstracker/controller/MinutesController.java
@@ -0,0 +1,28 @@
+package local.gdiaz.fitnesstracker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import local.gdiaz.fitnesstracker.model.Exercise;
+
+@Controller
+public class MinutesController {
+
+	@RequestMapping(value = "/addMinutes")
+	public String addMinutes(@ModelAttribute("exercise") Exercise exercise) {
+		
+		System.out.println(exercise.getMinutes());
+		
+		return "addMinutes";
+	}
+	
+	/*@RequestMapping(value = "/addMoreMinutes")
+	public String addMoreMinutes(@ModelAttribute("exercise") Exercise exercise) {
+		
+		System.out.println(exercise.getMinutes());
+		
+		return "addMinutes";
+	}*/
+	
+}
